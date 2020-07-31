6a67c13168307c89b67876d33a688d6a88f179b9
Khyber Lahori <klaho028@uottawa.ca>
2020-07-31 17:48:29 -0400

    Added selection sort

--- selectionsort.py
+++ selectionsort.py
@@ -7,16 +7,19 @@ upper_bound = 15
 for i in range(size):
      arr.append(randrange(lower_bound, upper_bound + 1))
 
+arr = [4, -13, -14, -2, 10]
 print("Orignial:", arr)
+
 # Selection Sort
 def selection_sort(arr):
     n = len(arr)
     current_min_index = 0
 
     for i in  range(n):
-        for j in range(i + 1, n):
+        for j in range(i, n):
             if arr[j] < arr[current_min_index]:
                 current_min_index = j
+            print(arr)
         arr[i], arr[current_min_index] = arr[current_min_index], arr[i]
 
     return arr
---
+++ sort_test.py
@@ -0,0 +1,20 @@
+import unittest
+from random import randrange
+
+import bubblesort
+import selectionsort
+
+
+class Validity(unittest.TestCase):
+    self.cases = []
+    self.arrays = 10
+    self.size = 30
+    self.lower = -15
+    self.upper = 15
+
+    def generate_testcases(self, numbers):
+        for i in range(self.arrays):
+            for j in range(self.size):
+                
+    def bubblesort_test(self, ):
+
